b354bc968ff71890a752c80557c71430fae560da
Ramjee Ganti <code@ramjeeganti.com>
2017-08-31 00:05:45 +0530

    added router link active capabilities

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,33 @@
-import React, {Component} from 'react';
-import {Nav, NavItem, Jumbotron} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Nav, NavItem, Jumbotron } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Home extends Component {
-
   render() {
     return (
       <div>
         <div className="header clearfix">
           <nav>
             <Nav bsStyle="pills" pullRight>
-              <NavItem href="/home" className="active" aria-label="presentation" >Home</NavItem>
-              <NavItem href="/addpost" aria-label="presentation">Add</NavItem>
-              <NavItem href="#" aria-label="presentation">Logout</NavItem>
+              <LinkContainer to="/home" activeClassName="active">
+                <NavItem aria-label="presentation">Home</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/addpost" activeClassName="active">
+                <NavItem aria-label="presentation">Add</NavItem>
+              </LinkContainer>
+              <LinkContainer to="#" activeClassName="active">
+                <NavItem aria-label="presentation">Logout</NavItem>
+              </LinkContainer>
             </Nav>
           </nav>
           <h3 className="text-muted">React Blog App</h3>
         </div>
-        <Jumbotron>
-          {this.props.children}
-        </Jumbotron>
+        <Jumbotron>{this.props.children}</Jumbotron>
 
-      <footer className="footer">
-        <p>&copy; 2017 Company, Inc.</p>
-      </footer>
-    </div>
+        <footer className="footer">
+          <p>&copy; 2017 Company, Inc.</p>
+        </footer>
+      </div>
     );
   }
 }
