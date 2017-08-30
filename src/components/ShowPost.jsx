5b03900d9963220bb2604927352bbab3155e0c8e
Ramjee Ganti <code@ramjeeganti.com>
2017-08-31 00:38:51 +0530

    added delete and update

--- src/components/ShowPost.jsx
+++ src/components/ShowPost.jsx
@@ -1,31 +1,39 @@
-import React, {Component} from 'react';
-import {Table, Glyphicon} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Table, Glyphicon } from 'react-bootstrap';
 import axios from 'axios';
 
 import Home from './Home';
 
-
 class ShowPost extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      posts:[]
-    }
+      posts: []
+    };
   }
 
   componentDidMount = () => {
     var self = this;
-    axios.post('http://localhost:9000/getpost',{
-    }).then((response) => {
-      self.setState({posts:response.data});
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
+    axios
+      .post('http://localhost:9000/getpost', {})
+      .then(response => {
+        self.setState({ posts: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-  
+  updatePost = id => {
+    console.log('update', id);
+  };
+
+  deletePost = id => {
+    console.log('delete', id);
+  };
+
   render() {
-    return(
+    return (
       <Home>
         <Table striped bordered condensed hover>
           <thead>
@@ -33,28 +41,33 @@ class ShowPost extends Component {
               <th>#</th>
               <th>Title</th>
               <th>Subject</th>
-              <th></th>
-              <th></th>  
+              <th />
+              <th />
             </tr>
           </thead>
           <tbody>
-            {
-              this.state.posts.map((post, index) => {
-                return <tr key={index}>
+            {this.state.posts.map((post, index) => {
+              return (
+                <tr key={index}>
                   <td>{post.id}</td>
                   <td>{post.title}</td>
                   <td>{post.subject}</td>
                   <td>
-                    <Glyphicon glyph="pencil"></Glyphicon>
+                    <Glyphicon
+                      glyph="pencil"
+                      onClick={this.updatePost.bind(this, post.id)}
+                    />
                   </td>
                   <td>
-                  <Glyphicon glyph="remove"></Glyphicon>
+                    <Glyphicon
+                      glyph="remove"
+                      onClick={this.deletePost.bind(this, post.id)}
+                    />
                   </td>
                 </tr>
-              })
-            }
+              );
+            })}
           </tbody>
-
         </Table>
       </Home>
     );
